a086b9c2b2698f08fb4b893c8fbba06f91473d18
irl-dan <97565471+irl-dan@users.noreply.github.com>
2023-03-19 00:18:39 -0400

    fix issues

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # git-gpt
+
+This is a javascript program written to run on node.js
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require("fs-extra");
 const path = require("path");
 const { execSync } = require("child_process");
 const dotenv = require("dotenv");
-const { Octokit } = require("@octokit/rest");
 const { Configuration, OpenAIApi } = require("openai");
 
 dotenv.config();
@@ -11,65 +10,60 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const UNIQUE_DELIMITER = "|||GPT4_DELIMITER|||";
-const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
 const MAX_ITERATIONS = 20;
 
 async function main() {
   const directory = process.cwd();
-
-  const context =
-    "You are working on a pull request for a project. You are midway through making changes according to the following Planned Changes. Included below is helpful context about the changes and the state of the code repository.";
   const topLevelGoal =
-    "Parameterize the request so that we can pass it in as an argument";
+    "Parameterize the `topLevelGoal` so that we can pass it in as a node js argument";
 
-  const { branchName } = await getBranchName({ topLevelGoal });
+  const { branch } = await getBranchName({ topLevelGoal });
+
+  let gameplan = "Inspect index.js and figure out where to go from there.";
+  let workingFile = "";
+  let commandOutputs = executeCommands(["ls", "cat index.js"]);
 
   // git checkout branch
-  console.log(`Checking out a new branch ${branchName}`);
-  execSync(`git checkout -b ${branchName}`, { encoding: "utf-8" });
+  console.log(`Checking out a new branch ${branch}`);
+  execSync(`git checkout -b ${branch}`, { encoding: "utf-8" });
 
   const iteration = 0;
   while (iteration < MAX_ITERATIONS) {
     console.log(`Starting iteration ${iteration}`);
-    const {
-      patch,
-      patchDescription,
-      gameplan,
-      commands,
-      workingFile,
-      complete,
-    } = await iterate({
+    let { patch, patchDescription, nextIteration, complete } = await iterate({
       topLevelGoal,
       gameplan,
-      readContents,
-      fileContent,
       commandOutputs,
+      workingFile,
     });
 
     // log response to patch file
-    logIteration(directory, branchName, iteration);
-
-    if (complete) {
-      console.log(`Top Level Task completed on iteration ${iteration}`);
-      break;
-    }
+    logIteration(directory, branch, iteration);
 
     if (patch) {
-      applyPatch(directory, branchName, iteration, patch);
+      applyPatch(directory, branch, iteration, patch);
     }
 
     if (patchDescription) {
       plannedChanges += patchDescription;
     }
 
-    if (commands) {
+    if (complete) {
+      console.log(`Top Level Task completed on iteration ${iteration}`);
+      break;
+    }
+
+    if (nextIteration?.commands) {
       commandOutputs = executeCommands(commands);
     }
 
-    if (workingFile) {
+    if (nextIteration?.workingFile) {
       const workingFilePath = path.join(directory, workingFile);
-      const workingFileContent = fs.readFileSync(workingFilePath, "utf-8");
+      workingFile = fs.readFileSync(workingFilePath, "utf-8");
+    }
+
+    if (nextIteration?.gameplan) {
+      gameplan = nextIteration.gameplan;
     }
 
     iteration++;
@@ -78,7 +72,7 @@ async function main() {
   const { commitMessage } = await getCommitMessage();
   const { changeLog } = await getChangeLog();
 
-  logCommit(directory, branchName, changeLog);
+  logCommit(directory, branch, changeLog);
 
   console.log("Committing changes and creating a new branch");
 
@@ -117,11 +111,11 @@ function executeCommands(commands) {
   return commandOutputs;
 }
 
-function applyPatch(directory, branchName, iteration, patch) {
+function applyPatch(directory, branch, iteration, patch) {
   const patchPath = path.join(
     directory,
     ".gpt-git",
-    branchName,
+    branch,
     iteration,
     "patch"
   );
@@ -133,11 +127,11 @@ function applyPatch(directory, branchName, iteration, patch) {
   });
 }
 
-function logIteration(directory, branchName, iteration) {
+function logIteration(directory, branch, iteration) {
   const iterationLogPath = path.join(
     directory,
     ".gpt-git",
-    branchName,
+    branch,
     iteration,
     "response.json"
   );
@@ -149,11 +143,11 @@ function logIteration(directory, branchName, iteration) {
   );
 }
 
-function logCommit(directory, branchName, changeLog) {
+function logCommit(directory, branch, changeLog) {
   const changeLogPath = path.join(
     directory,
     ".gpt-git",
-    branchName,
+    branch,
     "CHANGE_LOG.md"
   );
   fs.ensureFileSync(changeLogPath);
@@ -163,10 +157,13 @@ function logCommit(directory, branchName, changeLog) {
 async function iterate({
   topLevelGoal,
   gameplan,
-  readContents,
-  fileContent,
   commandOutputs,
+  workingFile,
 }) {
+  let workingFileContent = workingFile
+    ? fs.readFileSync(workingFile, "utf-8")
+    : "<no working file selected>";
+
   const gitStatusCommand = "git status --short";
   const gitStatus = execSync(gitStatusCommand, { encoding: "utf-8" });
 
@@ -174,6 +171,13 @@ async function iterate({
   const gitTree = execSync(gitTreeCommand, { encoding: "utf-8" });
 
   const readme = fs.readFileSync("README.md", "utf-8");
+
+  const formattedCommandOutputs = Object.entries(commandOutputs).reduce(
+    (acc, [command, output]) => {
+      return `${acc}\n\n>>>>>>>>>> ${command}\n\`\`\`\n${output}\n\`\`\`\n`;
+    },
+    ""
+  );
 
   const systemPrompt = `As GPT-4, you are an expert in software architecture, development, and analysis.\n
     You are presented with a Top Level Goal for which we need to make changes to the repository.\n\n
@@ -219,16 +223,10 @@ async function iterate({
     \`\`\`\n
 
     ### Command Stdout/StdErr\n
-    ${commandOutputs}\n\n
-
-    ### Relevant Context\n
-    ${readContents}\n\n
+    ${formattedCommandOutputs}\n\n
 
     ### Working File\n
-    ${fileContent}\n\n
-
-    ### Working File Context\n
-    ${fileContent}\n\n
+    ${workingFileContent}\n\n
     ================== End Relevant Context ==================\n\n
 
     It is now your job to make specific improvements and modifications according to the initial request. Consider the changes you are sure you want to make given what you know about the repository.\n\n
@@ -239,7 +237,7 @@ async function iterate({
         "nextIteration": {\n
             "gameplan": \${the above given Gameplan, modified to reflect only the remaining steps required to achieve the Top Level Goal after applying the above patch.},\n
             "commands": [\${one or more commands we should run after applying the above patch in service of learning about how to best apply the next Gameplan. Use commands like \`ls\` \`grep\`, \`cat\`, \`npm run test\`, \`npm run lint\`, and \`git\` to inspect the current state of the repository, search for terms, read files and directories, and run tests to see if the codebase is consistent. Be mindful of output format, run only commands that are immediately useful, keep them targetted. Configure commands to return only the necessary information to conserve space.}],\n
-            "workingFile": \${the next Working File we should open after applying the above patch in order to achieve the Top Level Goal.},\n
+            "workingFile": \${the path of the next Working File we should open after applying the above patch in order to achieve the Top Level Goal.},\n
         },\n
         "complete": \${\`true\` if you are confident that you have completed the Top Level Goal. \`false\` if you are not confident that you have completed the Top Level Goal. This is exlusive with the \`nextIteration\` field.}\n
     }\n
@@ -250,28 +248,24 @@ async function iterate({
     { role: "user", content: userPrompt },
   ];
 
-  const response = chat(messages);
+  const response = await chat(messages);
 
-  const {
-    patch,
-    patchDescription,
-    nextIteration: { gameplan, commands, workingFile },
-    complete,
-  } = JSON.parse(response);
+  const { patch, patchDescription, nextIteration, complete } =
+    JSON.parse(response);
 
   return {
     patch,
     patchDescription,
-    gameplan,
-    commands,
-    workingFile,
+    nextIteration,
     complete,
   };
 }
 
-async function getCommitDescription({ topLevelGoal }) {
+async function getCommitMessage({ topLevelGoal }) {
   const gitDiffCommand = "git diff";
   const gitDiff = execSync(gitDiffCommand, { encoding: "utf-8" });
+
+  const systemPrompt = `You are a helpful, accurate, knowledgable technical writer.`;
 
   const userPrompt = `### Top Level Commit Goal\n\n
     ${topLevelGoal}\n\n
@@ -282,7 +276,7 @@ async function getCommitDescription({ topLevelGoal }) {
     ${gitDiff}\n
     \`\`\`\n\n
 
-    Recommend a concise but accurate commit message for the above diff. Respond in the following template:
+    Recommend a concise but accurate commit message for the above diff. Respond only with a valid JSON Object. Escape any characters that need to be escaped. Format the response according to the following template:\n\n
     {\n
         "commitMessage": \${concise but accurate commit message that summarizes the above diff into a single, easy to parse sentence no more than 72 characters long.},\n
     }\n
@@ -293,7 +287,7 @@ async function getCommitDescription({ topLevelGoal }) {
     { role: "user", content: userPrompt },
   ];
 
-  const response = chat(messages);
+  const response = await chat(messages, "gpt-3.5-turbo");
 
   const { commitMessage } = JSON.parse(response);
   return { commitMessage };
@@ -302,6 +296,8 @@ async function getCommitDescription({ topLevelGoal }) {
 async function getChangeLog({ topLevelGoal }) {
   const gitDiffCommand = "git diff";
   const gitDiff = execSync(gitDiffCommand, { encoding: "utf-8" });
+
+  const systemPrompt = `You are a helpful, accurate, knowledgable technical writer.`;
 
   const userPrompt = `### Top Level Commit Goal\n\n
     ${topLevelGoal}\n\n
@@ -323,19 +319,21 @@ async function getChangeLog({ topLevelGoal }) {
     { role: "user", content: userPrompt },
   ];
 
-  const response = chat(messages);
+  const response = await chat(messages, "gpt-3.5-turbo");
 
   const { commitMessage } = JSON.parse(response);
   return { commitMessage };
 }
 
 async function getBranchName({ topLevelGoal }) {
+  const systemPrompt = `You are a helpful, accurate, knowledgable software engineer. You respond only with a valid JSON Object. Escape any characters that need to be escaped.`;
+
   const userPrompt = `### Top Level Goal\n\n
     ${topLevelGoal}\n\n
 
-    We're going to work on the above goal in a git branch. Recommend a git branch name. Respond in the following template:
+    We're going to work on the above goal in a git branch. Recommend a git branch name. Respond only with a valid JSON Object. Escape any characters that need to be escaped. Format the response according to the following template:\n\n
     {\n
-        "branchName": \${Recommend a specific git branch name that corresponds to the Top Level Goal. Use kebab case, use at least 30 characters},\n
+        "branch": \${Recommend a specific git branch name that corresponds to the Top Level Goal. Use kebab case, use at least 30 characters},\n
     }\n
   `;
 
@@ -344,22 +342,22 @@ async function getBranchName({ topLevelGoal }) {
     { role: "user", content: userPrompt },
   ];
 
-  const response = chat(messages);
+  const response = await chat(messages, "gpt-3.5-turbo");
 
   const { branch } = JSON.parse(response);
   return { branch };
 }
 
-async function chat(messages) {
+async function chat(messages, model = "gpt-4") {
   console.log(
-    `=====================Local to GPT-4>>>>>>>>>>>>>>>>>>>>>\n\n${JSON.stringify(
+    `=====================Local to GPT-4>>>>>>>>>>>>>>>>>>>>>\n${JSON.stringify(
       messages,
       null,
       2
-    )}========================================================`
+    )}\n========================================================`
   );
   const response = await openai.createChatCompletion({
-    model: "gpt-4",
+    model,
     messages,
     temperature: 1,
     n: 1,
@@ -368,76 +366,10 @@ async function chat(messages) {
   const content = response.data.choices[0].message.content;
 
   console.log(
-    `<<<<<<<<<<<<<<<<<<<<<<GPT-4 to Local:\n\n=====================${content}========================================================`
+    `<<<<<<<<<<<<<<<<<<<<<<GPT-4 to Local:=====================\n${content}\n========================================================`
   );
 
   return content;
-}
-
-async function changeSingleFile(markdown) {
-  const systemPrompt =
-    "As GPT-4, you are an expert in software development and code analysis. Follow the request at the bottom to provide insightful recommendations and helpful fixes for the code. Make changes to files only when it is essential for improving the code quality or fixing issues.";
-
-  const userPrompt = `Given the stuffed markdown context:
-
-  ### Context\n
-  ${context}\n\n
-  
-  ### Planned Changes\n
-  ${plannedChanges}\n\n
-  
-  ### Git Status\n
-  ${gitStatus}\n\n
-
-  ### Git Tree\n
-  ${gitTree}\n\n
-
-  ### Relevant Context\n
-  ${readContents}\n\n
-
-  ### Working File\n
-  ${fileContent}\n\n
-
-  ### Request\n
-  ${request}
-
-Carefully analyze the code and provide a recommended file change that addresses any issues, optimizes the code, or enhances its functionality. Ensure that your recommendations are precise, relevant, and actionable. Respond with the new file contents, followed by the unique delimiter:
-
-${UNIQUE_DELIMITER}
-
-Then, provide a compact pseudocode interface summary of the new version of the file, including public classes, method signatures, exported function signatures, and any other useful information for an external file import. This summary should be as compact as possible while still giving insight into the public-facing interfaces within the file.`;
-
-  const messages = [
-    { role: "system", content: systemPrompt },
-    { role: "user", content: userPrompt },
-  ];
-  return chat(messages);
-}
-
-async function createPullRequest(owner, repo, head, base, title, body) {
-  try {
-    const response = await octokit.rest.pulls.create({
-      owner,
-      repo,
-      head,
-      base,
-      title,
-      body,
-    });
-
-    if (response.status === 201) {
-      console.log(
-        `Pull request created successfully: ${response.data.html_url}`
-      );
-      return response.data.html_url;
-    } else {
-      console.error("Error creating pull request:", response);
-      return null;
-    }
-  } catch (error) {
-    console.error("Error creating pull request:", error);
-    return null;
-  }
 }
 
 main()
